3eb0a67cbb3d671f4e6ac9672011bc188f065f95
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-12 01:32:11 +0530

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[st]<=nums[mid]){
+                if(nums[st]<=target && nums[mid]>=target){
+                    end=mid-1;
+                }
+                else{
+                    st=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && nums[end]>=target){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
